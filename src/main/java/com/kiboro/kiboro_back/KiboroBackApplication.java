0ffbcb8a610c267e329f51162ca847261e0361c0
remy <hamedremy@hotmail.fr>
2025-09-25 16:25:48 +0200

    Initial commit

---
+++ src/main/java/com/kiboro/kiboro_back/KiboroBackApplication.java
@@ -0,0 +1,13 @@
+package com.kiboro.kiboro_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiboroBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiboroBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiboro/kiboro_back/KiboroBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiboro.kiboro_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiboroBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
