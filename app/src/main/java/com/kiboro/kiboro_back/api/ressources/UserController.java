84a8eecca6ec999bcad73c416b8fa6913626ec5d
hamed remy <43240309+remyHamed@users.noreply.github.com>
2025-10-13 18:26:50 +0200

    wip: docker settings

--- app/src/main/java/com/kiboro/kiboro_back/api/ressources/UserController.java
+++ app/src/main/java/com/kiboro/kiboro_back/api/ressources/UserController.java
@@ -1,19 +1,46 @@
 package com.kiboro.kiboro_back.api.ressources;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-// Annotation
-@Controller
+@RestController 
 public class UserController {
-    
-    @RequestMapping("/hello")
-    @ResponseBody
 
-    // Method
-    public String helloGFG()
+
+    @PostMapping("/user")
+    @ResponseBody
+    public String user()
     {
-        return "Hello GeeksForGeeks";
+        return "user";
     }
+
+
+
+    @PutMapping("/user/{id}")
+    @ResponseBody
+    public String hi(@PathVariable String id)
+    {
+        return "hi " + id.toString();
+    }
+    
+    @GetMapping("/user/{id}")
+    @ResponseBody
+    public String helloGFG(@PathVariable String id)
+    {
+        return "Hello GeeksForGeeks " + id.toString();
+    }
+
+    @DeleteMapping("/user/{id}")
+    @ResponseBody
+    public String deleteUser(@PathVariable String id)
+    {
+        return "Deleted user " + id.toString();
+    }
+
+
+
 }
